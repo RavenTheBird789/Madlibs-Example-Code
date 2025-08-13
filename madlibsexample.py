527b0fcb6a89e726af23de7b62706e93f162a2ad
Art3mis_XP <northwalker676@gmail.com>
2025-08-12 20:34:37 -0400

    Add files via upload

---
+++ madlibsexample.py
@@ -0,0 +1,147 @@
+# madlibs example code
+
+import random
+
+a = input("Would you like to play madlibs? (y/n): ")
+if a == "y":
+    def madlibs():
+        verb1 = input("Choose a verb: ")
+        verb2 = input("Choose a verb: ")
+        adj = input("Choose an adjective: ")
+        noun = input("Choose a noun: ")
+
+        sentence1 = f"I can't wait to {verb1} after school with my {adj} {noun} then {verb2} with my mom when I get home!";
+        sentence2 = f"Once it's summer I plan to {verb1} at the beach with my {noun} and {verb2} ice cream in my {adj} car.";
+        
+        b = [sentence1, sentence2]
+        print(random.choice(b));
+        
+        c = input("Would you like to play again? (y/n): ");
+        if c == "y":
+            madlibs();
+        elif c == "Y":
+            madlibs();
+        elif c == "yes":
+            madlibs();
+        elif c == "Yes":
+            madlibs();
+        elif c == "n":
+            print("Have a nice day!");
+        elif c == "no":
+            print("Have a nice day!");
+        elif c == "N":
+            print("Have a nice day!");
+        elif c == "No":
+            print("Have a nice day!");
+        else:
+            print("Invalid Input")
+    madlibs();
+elif a == "Y":
+    def madlibs():
+        verb1 = input("Choose a verb: ")
+        verb2 = input("Choose a verb: ")
+        adj = input("Choose an adjective: ")
+        noun = input("Choose a noun: ")
+
+        sentence1 = f"I can't wait to {verb1} after school with my {adj} {noun} then {verb2} with my mom when I get home!";
+        sentence2 = f"Once it's summer I plan to {verb1} at the beach with my {noun} and {verb2} ice cream in my {adj} car.";
+        
+        b = [sentence1, sentence2]
+        print(random.choice(b));
+        
+        c = input("Would you like to play again? (y/n): ");
+        if c == "y":
+            madlibs();
+        elif c == "Y":
+            madlibs();
+        elif c == "yes":
+            madlibs();
+        elif c == "Yes":
+            madlibs();
+        elif c == "n":
+            print("Have a nice day!");
+        elif c == "no":
+            print("Have a nice day!");
+        elif c == "N":
+            print("Have a nice day!");
+        elif c == "No":
+            print("Have a nice day!");
+        else:
+            print("Invalid Input")
+    madlibs();
+elif a == "yes":
+    def madlibs():
+        verb1 = input("Choose a verb: ")
+        verb2 = input("Choose a verb: ")
+        adj = input("Choose an adjective: ")
+        noun = input("Choose a noun: ")
+
+        sentence1 = f"I can't wait to {verb1} after school with my {adj} {noun} then {verb2} with my mom when I get home!";
+        sentence2 = f"Once it's summer I plan to {verb1} at the beach with my {noun} and {verb2} ice cream in my {adj} car.";
+        
+        b = [sentence1, sentence2]
+        print(random.choice(b));
+        
+        c = input("Would you like to play again? (y/n): ");
+        if c == "y":
+            madlibs();
+        elif c == "Y":
+            madlibs();
+        elif c == "yes":
+            madlibs();
+        elif c == "Yes":
+            madlibs();
+        elif c == "n":
+            print("Have a nice day!");
+        elif c == "no":
+            print("Have a nice day!");
+        elif c == "N":
+            print("Have a nice day!");
+        elif c == "No":
+            print("Have a nice day!");
+        else:
+            print("Invalid Input")
+    madlibs();
+elif a == "Yes":
+    def madlibs():
+        verb1 = input("Choose a verb: ")
+        verb2 = input("Choose a verb: ")
+        adj = input("Choose an adjective: ")
+        noun = input("Choose a noun: ")
+
+        sentence1 = f"I can't wait to {verb1} after school with my {adj} {noun} then {verb2} with my mom when I get home!";
+        sentence2 = f"Once it's summer I plan to {verb1} at the beach with my {noun} and {verb2} ice cream in my {adj} car.";
+        
+        b = [sentence1, sentence2]
+        print(random.choice(b));
+        
+        c = input("Would you like to play again? (y/n): ");
+        if c == "y":
+            madlibs();
+        elif c == "Y":
+            madlibs();
+        elif c == "yes":
+            madlibs();
+        elif c == "Yes":
+            madlibs();
+        elif c == "n":
+            print("Have a nice day!");
+        elif c == "no":
+            print("Have a nice day!");
+        elif c == "N":
+            print("Have a nice day!");
+        elif c == "No":
+            print("Have a nice day!");
+        else:
+            print("Invalid Input")
+    madlibs();
+elif a == "n":
+    print("Okay then, have a nice day!");
+elif a == "no":
+    print("Okay then, have a nice day!");
+elif a == "N":
+    print("Okay then, have a nice day!");
+elif a == "No":
+    print("Okay then, have a nice day!");
+else:
+    print("Invalid Input");
